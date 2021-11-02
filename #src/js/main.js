4fbc26ebe2a4385417cf78105fcf9abfabff8e48
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2021-11-02 17:42:43 +0300

    video page start

--- #src/js/main.js
+++ #src/js/main.js
@@ -21,10 +21,8 @@ burger.addEventListener('click',()=>{
     burger.classList.toggle('burger-active')
     if(burger.className.includes("burger-active")){
         menu.classList.add("header__menu-active")
-        langMenu.style.display='block'
     }else{
         menu.classList.remove("header__menu-active")
-        langMenu.style.display='none'
     }
 })
 
@@ -40,9 +38,15 @@ const swiper = new Swiper('.model__slider', {
     slidesPerView: "auto",
     spaceBetween: 13,
 });
+const videoSlider = new Swiper('.video__slider', {
+    wrapperClass: "video__wrapper",
+    slideClass:"video__slide",
+    slidesPerView: "auto",
+    spaceBetween: 30,
+});
 
 
-const map = document.getElementById('map');
+var map = document.getElementById('map');
 
 function initMap() {
   map = new google.maps.Map(map, {
@@ -53,9 +57,24 @@ function initMap() {
 }
 
 
-$.ajax('https://novias1986.bitrix24.ua/rest/1/z9lgewgqaftmtmxt/crm.contact.list.json?SELECT[]=NAME&SELECT[]=LAST_NAME&SELECT[]=EMAIL', {
+/*$.ajax('https://novias1986.bitrix24.ua/rest/1/z9lgewgqaftmtmxt/crm.contact.list.json?SELECT[]=NAME&SELECT[]=LAST_NAME&SELECT[]=EMAIL', {
     success: (data) => { 
         console.log(data)
      },
-    error: (err) => { /* обробка помилки */}
-});
+    error: (err) => {console.log(err)}
+});*/
+
+const bigImg = document.querySelector(".model__big img")
+const smallImg = document.querySelectorAll(".model__img img")
+
+smallImg.forEach(item=>{
+  item.addEventListener("click", ()=>{
+    smallImgSrc = item.src
+    bigImg.src = smallImgSrc
+  })
+})
+
+const video = document.querySelectorAll('.video__slide video')
+const playVideoBtn = document.querySelectorAll(".video__play")
+
+
