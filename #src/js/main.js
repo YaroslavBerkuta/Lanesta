a1cd111e2328351bd4b9bcaf1b752b4881c6c688
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2021-11-11 17:31:13 +0300

    modil

--- #src/js/main.js
+++ #src/js/main.js
@@ -29,6 +29,17 @@ burger.addEventListener('click',()=>{
 langMenu.addEventListener('click',()=>{
     langList.classList.toggle('visble__lang')
 })
+function blogSlider(){
+  const blogSwiper = new Swiper('.blog__slider', {
+    wrapperClass: "blog__wrapper",
+    slideClass:"blog__item",
+    slidesPerView: "auto",
+    spaceBetween: 13,
+});
+}
+
+
+
 
 main.contains(pagepiling) && body.classList.add("body-overflow")
 
@@ -48,21 +59,6 @@ const videoSlider = new Swiper('.video__slider', {
 
 var map = document.getElementById('map');
 
-function initMap() {
-  map = new google.maps.Map(map, {
-    center: { lat: 49.41832849437991, lng: 26.980854698857758 },
-    mapId: "1be87eee1d42f34d",
-    zoom: 5,
-  });
-}
-
-
-/*$.ajax('https://novias1986.bitrix24.ua/rest/1/z9lgewgqaftmtmxt/crm.contact.list.json?SELECT[]=NAME&SELECT[]=LAST_NAME&SELECT[]=EMAIL', {
-    success: (data) => { 
-        console.log(data)
-     },
-    error: (err) => {console.log(err)}
-});*/
 
 const bigImg = document.querySelector(".model__big img")
 const smallImg = document.querySelectorAll(".model__img img")
@@ -74,7 +70,69 @@ smallImg.forEach(item=>{
   })
 })
 
-const video = document.querySelectorAll('.video__slide video')
-const playVideoBtn = document.querySelectorAll(".video__play")
+
+function initMap() {
+  const center =  { lat: 49.41832849437991, lng: 26.980854698857758 }
+    map = new google.maps.Map(map, {
+        center: center,
+        mapId: "1be87eee1d42f34d",
+        zoom: 5,
+    });
+    
+    const tourStops = [
+      [{ lat: 51.41832849437991, lng: 26.980854698857758 }, "Boynton Pass"],
+      [{ lat: 60.41832849437991, lng: 26.980854698857758 }, "Airport Mesa"],
+      [{ lat: 54.41832849437991, lng: 26.980854698857758 }, "Chapel of the Holy Cross"],
+      [{ lat: 65.41832849437991, lng: 26.980854698857758 }, "Red Rock Crossing"],
+      [{ lat: 33.41832849437991, lng: 26.980854698857758 }, "Bell Rock"],
+      [{ lat: 49.41832849437991, lng: 26.980854698857758 }, "NOVIAS"],
+    ];
+
+    const infoWindow = new google.maps.InfoWindow();
+
+    var image = {
+      url:"https://www.svgrepo.com/show/171081/store.svg",
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    
+    tourStops.forEach(([position, title], i) => {
+      const marker = new google.maps.Marker({
+        position,
+        icon: image,
+        map,
+        title: `${title}`,
+        optimized: false,
+      });
+  
+      // Add a click listener for each marker, and set up the info window.
+      marker.addListener("click", () => {
+        infoWindow.close();
+        infoWindow.setContent(marker.getTitle());
+        infoWindow.open(marker.getMap(), marker);
+      });
+    });
+}
+
+const video = document.querySelectorAll(".video__slide")
+const playVideo = document.querySelectorAll(".video__slide button")
+
+video.forEach(item=>{
+  item.lastElementChild.addEventListener("click",()=>{
+    item.lastElementChild.setAttribute("style","display: none")
+    item.firstElementChild.setAttribute("controls","true")
+    item.firstElementChild.play()
+  })
+})
+
+console.log( window.innerWidth );
 
 
+if(window.innerWidth <= 768){
+  blogSlider()
+  const modelNav = document.querySelector(".model__nav")
+  const modelLeft = document.querySelector(".model__left")
+  modelLeft.insertBefore(modelNav, modelLeft.firstChild)
+}
